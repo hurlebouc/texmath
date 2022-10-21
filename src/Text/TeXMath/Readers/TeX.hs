d685ffedf2f8ca75c0e6d1c39f694fcc2491db7e
nyzzfqo9u <nyzzfqo9u@mozmail.com>
2022-10-21 08:53:23 +0200

    add continuedFraction

--- src/Text/TeXMath/Readers/TeX.hs
+++ src/Text/TeXMath/Readers/TeX.hs
@@ -724,6 +724,7 @@ binary c = do
      "\\frac"     -> EFraction NormalFrac <$> texToken <*> texToken
      "\\tfrac"    -> EFraction InlineFrac <$> texToken <*> texToken
      "\\dfrac"    -> EFraction DisplayFrac <$> texToken <*> texToken
+     "\\cfrac"    -> EFraction ContinuedFrac <$> texToken <*> texToken
      "\\binom"    -> do
        a <- texToken
        b <- texToken
